4876c0eaf99aa2ea6d10540ba0370fc4e7e3d969
BugfishX <92337142+BugfishX@users.noreply.github.com>
2021-11-28 02:25:23 +0100

    Update pir.py

--- pir.py
+++ pir.py
@@ -5,9 +5,11 @@ import os
 from multiprocessing import Process
 
 #def var
-PIR_Sensor = 23
+PIR_Sensor = 4
 display_power_on= "vcgencmd display_power 1"
 display_power_off= "vcgencmd display_power 0"
+count_motion = 0
+count_nomotion = 0
 
 #set GPIOs to GPIO-Number
 GPIO.setmode(GPIO.BCM)
@@ -18,10 +20,9 @@ GPIO.setup(PIR_Sensor, GPIO.IN)
 
 #function to start MM
 def start_MM ():
-    print ">> START MAGIC MIRROR <<"
+    print (">> START MAGIC MIRROR <<")
     start_mm = "cd MagicMirror; npm run start"
     os.system(start_mm)
-    print ">> MAGIC MIRROR STARTED <<"
     
 #MAIN until abort with Ctrl + C
 p1 = Process(target=start_MM)
@@ -29,16 +30,21 @@ p1.start()
 
 while True:
     input=GPIO.input(PIR_Sensor)
+    time.sleep(0.2) #checking interval
 
-#set time.sleep for how long display stays on/off at least
 #no motion so display turns off
     if input==0:
-        print ("Not Sensing Motion", time.strftime("%d.%m.%Y %H:%M:%S"))
-        os.system(display_power_off)
-        time.sleep(1.0)
-#motion detected so display turns on
+        print ("Not Sensing Motion ", count_nomotion, "x -", time.strftime("%d.%m.%Y %H:%M:%S"))
+        count_nomotion+=1
+        if count_nomotion>=3:
+            os.system(display_power_off)
+            count_motion=0
 
+#motion detected so display turns on
     elif input==1:              
-        print ("Motion Detected", time.strftime("%d.%m.%Y %H:%M:%S"))
-        os.system(display_power_on)
-        time.sleep(30.0)
+        print ("Motion Detected ", count_motion, "x -", time.strftime("%d.%m.%Y %H:%M:%S"))
+        count_motion+=1
+        if count_motion >=10:
+            os.system(display_power_on)
+            count_nomotion=0
+            time.sleep(60.0) #how long display stays on at least
